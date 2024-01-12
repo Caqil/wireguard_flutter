934e588c2d9421f3434ecdfaff0c240cb922ec01
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2024-01-12 14:44:21 -0300

    chore: Update instructions and example app

--- README.md
+++ README.md
@@ -1,18 +1,93 @@
 # wireguard_flutter
 
-forks from [mysteriumnetwork](https://github.com/mysteriumnetwork/wireguard_dart/) tunnel
 A flutter plugin to setup and control VPN connection via [Wireguard](https://www.wireguard.com/) tunnel.
 
-|             | Android | iOS   | macOS | Windows | Linux |
-| ----------- | ------- | ----- | ----- | ------- | ----- |
-| **Support** | 21+     | 15.0+ | 12+   | 7+      | Any   |
+- [Usage](#usage)
+  - [Initialize](#initialize)
+  - [Connect](#connect)
+  - [Disconnect](#disconnect)
+  - [Stage](#stage)
+- [Supported Platforms](#supported-platforms)
+
 
 ## Usage
 
 To use this plugin, add `wireguard_flutter` or visit [Flutter Tutorial](https://flutterflux.com/).
 
-"WireGuard" is a registered trademark of Jason A. Donenfeld.
+```
+flutter pub add wireguard_flutter
+```
 
+### Initialize
+
+Initialize a wireguard instance with a name using `initialize`:
+
+```dart
+final wireguard = WireGuardFlutter();
+
+// initialize the interface
+await wireguard.initialize(interfaceName: 'wg0');
+```
+
+and declare the `.conf` data:
+```dart
+const String conf = '''[Interface]
+PrivateKey = 0IZmHsxiNQ54TsUs0EQ71JNsa5f70zVf1LmDvON1CXc=
+Address = 10.8.0.4/32
+DNS = 1.1.1.1
+
+
+[Peer]
+PublicKey = 6uZg6T0J1bHuEmdqPx8OmxQ2ebBJ8TnVpnCdV8jHliQ=
+PresharedKey = As6JiXcYcqwjSHxSOrmQT13uGVlBG90uXZWmtaezZVs=
+AllowedIPs = 0.0.0.0/0, ::/0
+PersistentKeepalive = 0
+Endpoint = 38.180.13.85:51820''';
+```
+
+For more info on the configuration data, see [the documentation](https://man7.org/linux/man-pages/man8/wg-quick.8.html) with examples.
+
+### Connect
+
+After initializing, connect using `startVpn`:
+
+```dart
+await wireguard.startVpn(
+  serverAddress: address, // the server address (e.g 'demo.wireguard.com:51820')
+  wgQuickConfig: conf, // the quick-config file
+  providerBundleIdentifier: 'com.example', // your app identifier
+);
+```
+
+### Disconnect
+
+After connecting, disconnect using `stopVpn`:
+
+```dart
+await wireguard.stopVpn();
+```
+
+### Stage
+
+Listen to stage change using `vpnStageSnapshot`:
+
+```dart
+wireguard.vpnStageSnapshot.listen((event) {
+  debugPrint("status changed $event");
+});
+```
+
+Or get the current stage using `getStage`:
+
+```dart
+final stage = await wireguard.stage();
+```
+
+## Supported Platforms
+
+|             | Android | iOS   | macOS | Windows | Linux |
+| ----------- | ------- | ----- | ----- | ------- | ----- |
+| **Version** | SDK 21+     | 15.0+ | 12+   | 7+      | Any   |
 
 ### Windows
 
@@ -33,3 +108,9 @@ For other Linux distros, see [this](https://www.wireguard.com/install/).
 ## FAQ & Troubleshooting
 
 On Linux, you may receive the error `resolvconf: command not found`. This is because wireguard tried to adjust the nameserver. Make sure to install `openresolv` or not provide the "DNS" field.
+
+---
+
+"WireGuard" is a registered trademark of Jason A. Donenfeld.
+
+Fork from [mysteriumnetwork](https://github.com/mysteriumnetwork/wireguard_dart/) tunnel.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:wireguard_flutter/wireguard_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WireGuard Example App'),
+        ),
+        body: const MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -15,22 +24,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final wireGuardFlutter = WireGuardFlutter();
+  final wireguard = WireGuardFlutter();
 
   late String name;
 
   @override
   void initState() {
     super.initState();
-    wireGuardFlutter.vpnStageSnapshot().listen((event) {
-      debugPrint("status changed $event");
-    });
+//    wireguard.vpnStageSnapshot.listen((event) {
+//      debugPrint("status changed $event");
+//    });
     name = 'wg_vpn';
   }
 
   Future<void> initialize() async {
     try {
-      await wireGuardFlutter.initialize(interfaceName: name);
+      await wireguard.initialize(interfaceName: name);
       debugPrint("initialize success");
     } catch (error, stack) {
       debugPrint("failed to initialize: $error\n$stack");
@@ -39,7 +48,7 @@ class _MyAppState extends State<MyApp> {
 
   void startVpn() async {
     try {
-      await wireGuardFlutter.startVpn(
+      await wireguard.startVpn(
         serverAddress: '167.235.55.239:51820',
         wgQuickConfig: conf,
         providerBundleIdentifier: 'com.billion.wireguardvpn.WGExtension',
@@ -51,7 +60,7 @@ class _MyAppState extends State<MyApp> {
 
   void disconnect() async {
     try {
-      await wireGuardFlutter.stopVpn();
+      await wireguard.stopVpn();
     } catch (e, str) {
       debugPrint('Failed to disconnect $e\n$str');
     }
@@ -59,96 +68,89 @@ class _MyAppState extends State<MyApp> {
 
   void getStatus() {
     debugPrint("getting stage");
-    wireGuardFlutter.stage().then((value) {
+    wireguard.stage().then((value) {
       debugPrint("stage $value");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('WireGuard Example App'),
-        ),
-        body: Container(
-          constraints: const BoxConstraints.expand(),
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: initialize,
-                style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(const Size(100, 50)),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                    overlayColor: MaterialStateProperty.all<Color>(
-                        Colors.white.withOpacity(0.1))),
-                child: const Text(
-                  'initialize',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: startVpn,
-                style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(const Size(100, 50)),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                    overlayColor: MaterialStateProperty.all<Color>(
-                        Colors.white.withOpacity(0.1))),
-                child: const Text(
-                  'Connect',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: disconnect,
-                style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(const Size(100, 50)),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                    overlayColor: MaterialStateProperty.all<Color>(
-                        Colors.white.withOpacity(0.1))),
-                child: const Text(
-                  'Disconnect',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: getStatus,
-                style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(const Size(100, 50)),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blueAccent),
-                    overlayColor: MaterialStateProperty.all<Color>(
-                        Colors.white.withOpacity(0.1))),
-                child: const Text(
-                  'Get status',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: initialize,
+            style: ButtonStyle(
+                minimumSize:
+                    MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'initialize',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: startVpn,
+            style: ButtonStyle(
+                minimumSize:
+                    MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Connect',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: disconnect,
+            style: ButtonStyle(
+                minimumSize:
+                    MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Disconnect',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: getStatus,
+            style: ButtonStyle(
+                minimumSize:
+                    MaterialStateProperty.all<Size>(const Size(100, 50)),
+                padding: MaterialStateProperty.all(
+                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueAccent),
+                overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.white.withOpacity(0.1))),
+            child: const Text(
+              'Get status',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
