e2ea7bfcdf0f85422a62a6dbc56945d257db2442
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2024-01-11 22:23:01 -0300

    feat: implement other methods

--- lib/linux/wireguard_flutter_linux.dart
+++ lib/linux/wireguard_flutter_linux.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
@@ -8,6 +9,13 @@ import '../wireguard_flutter.dart';
 class WireGuardFlutterLinux extends WireGuardFlutter {
   String? name;
   File? configFile;
+
+  String? _stage;
+  final _stageController = StreamController<String>.broadcast();
+  void _setStage(String stage) {
+    _stage = stage;
+    _stageController.add(stage);
+  }
 
   @override
   Future<void> initialize({
@@ -25,44 +33,31 @@ class WireGuardFlutterLinux extends WireGuardFlutter {
     String? localizedDescription,
     String? win32ServiceName,
   }) async {
+    _setStage(WireGuardFlutter.vpnPrepare);
     final tempDir = await getTemporaryDirectory();
     configFile = await File(
       '${tempDir.path}${Platform.pathSeparator}$name.conf',
     ).create();
     await configFile!.writeAsString(wgQuickConfig);
 
-    // final result = await Process.start('wg-quick', ['up', configFile!.path],
-    //    runInShell: true);
-    // print(result.stdout);
-    // result.stdin;
-
-    var shell = Shell();
-
-    await shell.run('''
-
-wg-quick up ${configFile!.path}
-
-''');
+    _setStage(WireGuardFlutter.vpnConnecting);
+    final shell = Shell();
+    await shell.run('wg-quick up ${configFile!.path}');
+    _setStage(WireGuardFlutter.vpnConnected);
   }
 
   @override
   Future<void> stopVpn() async {
     assert(configFile != null, 'Not started');
-    // final result = await Process.run('wg-quick', ['down', configFile!.path]);
-    // print(result.stdout);
-
-    var shell = Shell();
-
-    await shell.run('''
-
-wg-quick down ${configFile!.path}
-
-''');
+    _setStage(WireGuardFlutter.vpnDisconnecting);
+    final shell = Shell();
+    await shell.run('wg-quick down ${configFile!.path}');
+    _setStage(WireGuardFlutter.vpnDisconnected);
   }
 
   @override
-  Future<String> stage() async => 'bla';
+  Future<String> stage() async => _stage ?? WireGuardFlutter.vpnNoConnection;
 
   @override
-  Stream<String> vpnStageSnapshot() => Stream.value('bla');
+  Stream<String> vpnStageSnapshot() => _stageController.stream;
 }
